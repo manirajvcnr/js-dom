aece75a0c11e7e26e579356ba9f81e1480ac735b
manirajvcnr <cnrakashcnr@gmail.com>
2022-08-22 16:36:51 +0530

    add-file.

---
+++ classname/script.js
@@ -0,0 +1,5 @@
+// classname
+const collection = document.getElementsByClassName("example color");
+collection[0].style.backgroundColor = "red";
+
+collection[1].innerHTML="Hello India";
---
+++ id/script.js
@@ -0,0 +1,11 @@
+// getElementById
+
+const element=document.getElementById("demo");
+element.style.color="blue";
+
+// change the content
+// element.innerHTML="info by id";
+
+// direct change
+// document.getElementById("demo").style.color = "red";
+
